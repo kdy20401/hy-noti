164c5fff2e2fb8f0957daef7a071fb5e59a02f7b
강덕영 <2016025078@hanyang.ac.kr>
2021-11-03 23:24:58 +0900

    deploy test

--- backend/crawling/selenium/portal.py
+++ backend/crawling/selenium/portal.py
@@ -1,11 +1,7 @@
 import os, sys, time, boto3
 from datetime import datetime
-
-# mongodb
 from pymongo import MongoClient, ASCENDING
 from pymongo.errors import DuplicateKeyError
-
-# crawling
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
